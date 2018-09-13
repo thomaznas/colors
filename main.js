406e5332467084f3b46c654241cbda80aa730539
Thomaz <luiz.thomaz.nas@gmail.com>
2018-09-13 15:25:21 -0400

    class solution

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 //----------------------------------
 //          YOUR CODE
 //----------------------------------
+var counter = 0;
+
+for (var i=0; i < 81; i++) {
+
+    y = i*15;
+
+    for (var j=0; j < 53; j++) {
+        x = j*15;
+        create(x,y,colors[counter]);
+        counter++;
+    }
+
+}
